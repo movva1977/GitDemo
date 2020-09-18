eebd0056a37bd073cedc36802f11c90e71aa87f3
Chandrasekhar Movva <movva77@gmail.com>
2020-09-18 19:21:42 +0530

    private class changes

--- text.java
+++ text.java
@@ -1,18 +1,15 @@
-public class gitstuff{
-}
-public class gitstuff1{
-}
-public class gitstuff2{
-}
-public class gitstuff3{
-}
-public class gitstuff4{
-}
-public class gitstuff5{
-}
-public class gitstuff6{
-}
-public class gitstuff7{
-}
-public class gitstuff8{
-}
+Private Class Classname1{
+Statements..
+Statements..Statements..
+Statements..
+Statements..}
+Private Class Classname2{
+Statements..
+Statements..Statements..
+Statements..
+Statements..}
+Private Class Classname3{
+Statements..
+Statements..Statements..
+Statements..
+Statements..}
