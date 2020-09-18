061c665d87dfceceb373653644be10bf744e9d4e
Chandrasekhar Movva <movva77@gmail.com>
2020-09-18 18:02:57 +0530

    Architect changes from different time zone

--- text.java
+++ text.java
@@ -6,3 +6,13 @@ public class gitstuff2{
 }
 public class gitstuff3{
 }
+public class gitstuff4{
+}
+public class gitstuff5{
+}
+public class gitstuff6{
+}
+public class gitstuff7{
+}
+public class gitstuff8{
+}
